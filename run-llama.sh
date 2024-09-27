9e1c0fff1531bd5f9725440714625d697e2f4876
Yuchen <124506155+yuchen-ty@users.noreply.github.com>
2024-09-27 12:52:55 +0800

    Update run-llama.sh

--- run-llama.sh
+++ run-llama.sh
@@ -18,7 +18,7 @@ model_name='llama-7b'
 seed=0
 ns=5200
 
-torchrun --nproc_per_node=2 --master_port=${port} main.py \
+torchrun --nproc_per_node=8 --master_port=${port} main.py \
         --model_name_or_path "huggyllama/llama-7b" \
         --data_path ${data_path} \
         --p_data_path ${p_data_path} --p_seed ${seed} \
