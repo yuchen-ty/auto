3122b9ef5ce236df774df1d70c362b51e50c3a99
Yuchen <124506155+yuchen-ty@users.noreply.github.com>
2024-09-27 12:52:26 +0800

    Update run-gptj.sh

--- run-gptj.sh
+++ run-gptj.sh
@@ -18,7 +18,7 @@ model_name='gpt-j'
 seed=0
 ns=5200
 
-torchrun --nproc_per_node=2 --master_port=${port} main.py \
+torchrun --nproc_per_node=8 --master_port=${port} main.py \
         --model_name_or_path "EleutherAI/gpt-j-6b" \
         --data_path ${data_path} \
         --p_data_path ${p_data_path} --p_seed ${seed} \
